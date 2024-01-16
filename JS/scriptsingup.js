94d4c72d0358699393302f905e6ba3d18e4c64ec
Hugo Amadio <hugo_amadio@hotmail.com>
2024-01-16 16:39:15 -0300

    Pagina singup pronta cadastrando usuarios em um array

---
+++ JS/scriptsingup.js
@@ -0,0 +1,23 @@
+let cadastro = document.getElementById("cadastrar")
+let usuariosCadastrados = []
+
+cadastro.addEventListener("click", function(){
+    event.preventDefault();
+    console.log("Botão clicado")
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    let confirmaSenha = document.getElementById("confirmaSenha").value;
+
+    if(senha != confirmaSenha){
+        alert("As senhas não são iguais!")
+    } else {
+        let novoUsuario = {
+            nome: nome,
+            email: email,
+            senha: senha
+        };
+        usuariosCadastrados.push(novoUsuario)
+        console.log(usuariosCadastrados);
+    }
+})
