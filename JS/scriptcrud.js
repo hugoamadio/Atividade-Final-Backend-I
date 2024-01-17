1ccd9488cb0ef0af623bf76f616e8dacb31afa4f
Hugo Amadio <hugo_amadio@hotmail.com>
2024-01-17 13:30:19 -0300

    Configurado a pagina crud.html e configurado para salvar recados no localstorage

--- JS/scriptcrud.js
+++ JS/scriptcrud.js
@@ -1,8 +1,32 @@
 let novoReacado = document.getElementById("novoRecado")
+let novoRecadoModal = document.getElementById("btnrecadomodal")
+
 
 novoReacado.addEventListener("click", function () {
     event.preventDefault();
     console.log("Botao novo recado pressionado!!")
+})
 
+
+novoRecadoModal.addEventListener("click", function () {
+    event.preventDefault();
+    console.log("Botao novo recado modal pressionado!!")
+
+
+    let textoRecado = document.getElementById("recado").value
+    let listaArmazenamentoRecados = localStorage.getItem("listaArmazenamentoRecados")
+    if (!listaArmazenamentoRecados) {
+        listaArmazenamentoRecados = [] //Não utilizar let para não redeclarar
+    } else {
+        listaArmazenamentoRecados = JSON.parse(listaArmazenamentoRecados)
+    }
+
+    let objetoRecado = { recado: textoRecado }
+
+    listaArmazenamentoRecados.push(objetoRecado)
+    let salvarRecado = JSON.stringify(listaArmazenamentoRecados)
+    localStorage.setItem("listaArmazenamentoRecados", salvarRecado)
+    console.log(salvarRecado)
+    
 
 })
